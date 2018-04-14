1437da5fcaae836e398eec7a14b5a1a81339f045
Kasun Kodagoda <kvkrusl@gmail.com>
2018-04-14 17:33:34 +0530

    Configured inversify to use the new types

--- buildtask/SlackNotification/inversify.config.ts
+++ buildtask/SlackNotification/inversify.config.ts
@@ -1,6 +1,22 @@
 import { Container } from 'inversify';
+
+import { ITaskInput } from './interfaces/ITaskInput';
+import { ISlackClient } from './interfaces/ISlackClient';
+import { ISlackFileUpload } from './interfaces/ISlackFileUpload';
+import { ISlackChatMessage } from './interfaces/ISlackChatMessage';
+
 import TYPES from './types';
 
-let container = new Container();
+import { TaskInput } from './TaskInput';
+import { SlackClient } from './SlackClient';
+import { SlackFileUpload } from './SlackFileUpload';
+import { SlackChatMessage } from './SlackChatMessage';
+
+const container = new Container();
+
+container.bind<ITaskInput>(TYPES.ITaskInput).to(TaskInput).inSingletonScope();
+container.bind<ISlackClient>(TYPES.ISlackClient).to(SlackClient).inSingletonScope();
+container.bind<ISlackChatMessage>(TYPES.ISlackChatMessage).to(SlackChatMessage).inTransientScope();
+container.bind<ISlackFileUpload>(TYPES.ISlackFileUpload).to(SlackFileUpload).inTransientScope();
 
 export default container;
--- buildtask/SlackNotification/types.ts
+++ buildtask/SlackNotification/types.ts
@@ -1,5 +1,13 @@
-let TYPES = {
+import { ISlackFileUpload } from './interfaces/ISlackFileUpload';
+import { ISlackChatMessage } from './interfaces/ISlackChatMessage';
+import { ISlackClient } from './interfaces/ISlackClient';
+import { ITaskInput } from './interfaces/ITaskInput';
 
+const TYPES = {
+    ITaskInput: Symbol('ITaskInput'),
+    ISlackClient: Symbol('ISlackClient'),
+    ISlackChatMessage: Symbol('ISlackChatMessage'),
+    ISlackFileUpload: Symbol('ISlackFileUpload')
 };
 
 export default TYPES;
