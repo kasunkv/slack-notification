78e73ee510da16c086cd0e8ae3d157aa2d6e8c70
Kasun Kodagoda <kvkrusl@gmail.com>
2018-06-03 12:04:42 +0530

    Added new constants to the task

--- buildtask/SlackNotification/Constants.ts
+++ buildtask/SlackNotification/Constants.ts
@@ -2,3 +2,8 @@ export class NotificationType {
     static CHAT_MESSAGE: string = 'ChatMessage';
     static FILE_UPLOAD: string = 'UploadFile';
 }
+
+export class DestinationType {
+    static CHANNEL: string = 'Channel';
+    static DIRECT_MESSAGE: string = 'DirectMessage';
+}
