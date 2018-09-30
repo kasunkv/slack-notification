950dd28e85d7a98ef82e35a107375181a7e016fc
Kasun Kodagoda <kvkrusl@gmail.com>
2018-09-30 11:25:03 +0530

    updated the formatMultilineText() method

--- buildtask/SlackNotification/TaskInput.ts
+++ buildtask/SlackNotification/TaskInput.ts
@@ -226,7 +226,8 @@ export class TaskInput implements ITaskInput {
 
     formatMultilineText(input: string): string {
         return input
-            .replace(new RegExp('\n', 'g'), '\'\n\'')
+            .replace(new RegExp('\n', 'g'), '`n')
+            .replace(new RegExp('\t', 'g'), '`t')
             .replace(new RegExp('`n', 'g'), '\n')
             .replace(new RegExp('`t', 'g'), '\t');
     }
