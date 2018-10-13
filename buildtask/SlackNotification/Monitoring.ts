4c841eec3246e70e44d21b808afd32adcde9de40
Kasun Kodagoda <kvkrusl@gmail.com>
2018-10-13 23:42:27 +0530

    Added debug logs to the Monitoring.ts

--- buildtask/SlackNotification/Monitoring.ts
+++ buildtask/SlackNotification/Monitoring.ts
@@ -1,13 +1,22 @@
 import * as sentry from '@sentry/node';
 import { inject, injectable } from 'inversify';
 
+import TYPES from './di/types';
 
 import { IMonitoring } from './interfaces/IMonitoring';
 import { SentryEvent } from '@sentry/node';
+import { ILogger } from './interfaces/ILogger';
 
 @injectable()
 export class Monitoring implements IMonitoring {
+    private _logger: ILogger;
+
+    constructor(@inject(TYPES.ILogger) logger: ILogger) {
+        this._logger = logger;
+    }
+
     configure(): void {
+        this._logger.logDebug('[Monitoring.configure()] Configure called');
         sentry.init({
             dsn: 'SENTRY_DSN',
             release: 'TASK_RELEASE_VERSION'
@@ -15,6 +24,7 @@ export class Monitoring implements IMonitoring {
     }
 
     captureException(error: any): void {
+        this._logger.logDebug(`[Monitoring.captureException()] CaptureException called. Error=${error.message || error}`);
         sentry.captureException(error);
     }
 
